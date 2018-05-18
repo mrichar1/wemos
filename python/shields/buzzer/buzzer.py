ecf6e1c9e704c462b9937c96d9395d2d4ee82d81
Matthew Richardson <m.richardson@ed.ac.uk>
2018-05-18 21:07:28 +0100

    Make buzzer more exciting!

--- python/shields/buzzer/buzzer.py
+++ python/shields/buzzer/buzzer.py
@@ -1,11 +1,19 @@
 from machine import Pin, PWM
-from time import sleep
+import time
 
 beep_pin = Pin(14)
 
-while True:
-    beeper = PWM(beep_pin, freq=600, duty=868)
-    sleep(1)
-    beeper.deinit()
-  
+beeper = PWM(beep_pin)
+beeper.duty(868)
 
+def beeps(i):
+    beeper.freq(i)
+    time.sleep_ms(5)
+
+
+for i in range(100,1000):
+    beeps(i)
+for i in range(1000,100,-1):
+    beeps(i)
+
+beeper.deinit()
